137d209a9061be240b19da3a51257f365e94a55e
AustinSpinazze <austin.spinazze@perficient.com>
2020-02-10 10:03:29 -0600

    Add new components and styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,141 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+Stateful vs. Stateless Components - stateful components a component that manages state. Stateless component has no internal state management. It is good practice to make as many
+stateless (or dumb components) 
+App.js is considered a stateful component (or container components because they contain the state of your components). We only want to have a couple of these components because
+it depends on the size of your app. You restrict yourself to few stateful (container components) and many stateless (presentational components) because this makes your app easier
+to maintain and manage. You have a clear flow of data and it is very clear where your main logic sits and where your data changes
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Person.js is considered a stateless component because they have no internal logic. They are presentational components because typically
+they only get and present data.
+*/
+
+// import React, { useState } from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+// const App = props => {
+    // const [ personsState, setPersonsState ] = useState({
+    //   persons: [
+    //     { name: 'Austin', age: '25'},
+    //     { name: 'Alexis', age: '23'},
+    //     { name: 'Nick', age: '31'}
+    //   ],
+      // Other state is not affected by switch handler because it specifies 
+      // persons and does not do a blanket change on all of state
+      /* What are the only two things that change the cause React to update DOM?
+      *  1. Changing states 
+      *  2. Props
+      */
+    // });
+
+    state = {
+      persons: [
+        { name: 'Austin', age: 25 },
+        { name: 'Alexis', age: 24 },
+        { name: 'Nick', age: 31 }
+      ],
+      otherState: 'some other value'
+    }
+
+    // const [otherState, setOtherState] = useState('some other Value');
+
+    // console.log(personsState, otherState);
+
+    // const switchNameHandler = () => {
+    //   console.log('Was clicked!');
+    //   Don't do this muy no bueno this.state.persons[0].name = 'Augustus'
+    //   setPersonsState({
+    //     persons: [
+    //       { name: 'Augustus', age: '25'},
+    //       { name: 'Alexis', age: '23'},
+    //       { name: 'Nick', age: '105'}
+    //     ],
+    //     otherState: personsState.otherState
+    //   } )
+    // }
+
+    // return (
+    //   <div className="App">
+    //     <h1>Hi, I'm a React App</h1>
+    //     <p>This is really working!</p>
+    //     {/* When calling function in an even don't use () at the end unless you want
+    //     it to run on page load */}
+    //     <button onClick={this.switchNameHandler}>Switch Name</button>
+    //     {/* Person can be copied multipe times and can be used anywhere you import it into the project*/}
+    //     <Person 
+    //       name={personsState.persons[0].name} 
+    //       age={personsState.persons[0].age}/>
+    //     <Person 
+    //       name={personsState.persons[1].name} 
+    //       age={personsState.persons[1].age}
+    //       click={this.switchNameHandler}> My Hobbies: Drawing, Snuggling, Netflixing</Person>
+    //     <Person 
+    //       name={personsState.persons[2].name} 
+    //       age={personsState.persons[2].age}/>
+    //   </div>
+    // )
+    // not html but jsx and then is coverted to html by the browser
+    // can only have one root element (div, etc.) but can have adjacent elements
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+
+    // We can also pass values to our function by using new name for instance
+    switchNameHandler = (newName) => {
+      // console.log('Was clicked!');
+      // DON'T DO THIS: this.state.persons[0].name = 'Austin';
+      this.setState( {
+        persons: [
+          { name: newName, age: 26 },
+          { name: 'Alex', age: 24 },
+          { name: 'Nicholas', age: 31 }
+        ]
+      } )
+    }
+  
+    nameChangedHandler = (event) => {
+      this.setState( {
+        persons: [
+          { name: 'Augustus', age: 26 },
+          { name: event.target.value, age: 24 },
+          { name: 'Nicholas', age: 31 }
+        ]
+      } )
+    }
+
+    render () {
+
+      const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      };
+      
+      return (
+        <div className="App">
+          <h1>Hi, I'm a React App</h1>
+          <p>This is really working!</p>
+          {/* Creating an anonomyous function to pass a parameter is not as efficent as just using bind */}
+          <button style={style} onClick={() => this.switchNameHandler('Augustus!!')}>Switch Name</button>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            //You can pass methods as props so that you can call a method which might change the state in another component that may not have or should not have access to a certain state
+            click={this.switchNameHandler.bind(this, 'Augustus!!!')}
+            //We call this, 'String' because this ^ controls what the this inside the function will refer to and binds it here
+            changed={this.nameChangedHandler}> My Hobbies: Drawing, Snuggling, Netflixing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age} />
+        </div>
+      );
+      // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Person.css'
+
+//ES6 declaring function
+// function person() {
+//     return(
+//         <h2></h2>
+//     );
+// }
+
+//ES5 declaring function
+// var person = function() {
+
+// }
+
+// ES5 declaring const function
+// const person = function() {
+
+// }
+
+//ES6 declaring const function - better for | this | keyword
+/* 
+*   When using class based components instead of function bases component
+*   like belowe you would use this.props for Example
+*   class Person extends Component {
+*     render() {
+*       return <p>My name is {this.props}</p>
+*     }    
+*   }
+*/
+const person = (props) => {
+    // In order to use javascript code in JSX use sing curly braces
+    // This allows the output of dynamic content with our JSX content
+    return (
+    <div className="Person">
+        <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+        {/* We use props.children when we are trying to pass children elements
+        (elements inside elements) directly into the element output */}
+        <p>{props.children}</p>
+        <input type="text" onChange={props.changed} value={props.name}></input>
+    </div>
+    );
+}
+
+export default person;
